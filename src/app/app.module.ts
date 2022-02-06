2f58f1c17197479ca291805f3d994e19e80bda84
Gerald Nnebe <34799566+SabreFoxx@users.noreply.github.com>
2022-02-07 00:20:16 +0100

    Country item component closes #8 closes #4
    * use Reloadly's for branding
    
    * initial compoment setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LandingComponent } from './landing/landing.component';
+import { ComponentModule } from './components/component.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
