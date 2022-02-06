1cdef7a85653ea4d1eb560bb91e57269e3938bb3
SabreFoxx <geraldnebs@gmail.com>
2022-02-07 00:17:58 +0100

    initial compoment setup

---
+++ src/app/components/component.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountryCardComponent } from './country-card/country-card.component';
+
+@NgModule({
+  declarations: [CountryCardComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CountryCardComponent
+  ]
+})
+export class ComponentModule { }
