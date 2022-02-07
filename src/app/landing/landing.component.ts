0acd3db20632935cf0c091c4bad4c33ecedfaacb
SabreFoxx <geraldnebs@gmail.com>
2022-02-07 19:30:10 +0100

    finish landing page skeleton

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import SwiperCore, { SwiperOptions, Pagination } from 'swiper';
 
 SwiperCore.use([Pagination]);
@@ -6,25 +6,19 @@ SwiperCore.use([Pagination]);
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LandingComponent implements OnInit {
   swiperConfig: SwiperOptions;
 
   constructor() {
     this.swiperConfig = {
-      effect: 'coverflow',
       grabCursor: true,
-      slidesPerView: 'auto',
+      slidesPerView: 4,
       pagination: true,
       allowTouchMove: true,
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: false
-      }
+      spaceBetween: 5
     }
   }
 
