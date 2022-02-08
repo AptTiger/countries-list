df64f7224d23a023d3bb64f513209b7688d1390b
Gerald Nnebe <34799566+SabreFoxx@users.noreply.github.com>
2022-02-08 22:07:56 +0100

    implement search and filter closes #22

--- src/app/components/search-filter/search-filter.component.ts
+++ src/app/components/search-filter/search-filter.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { debounceTime, map, Observable, ReplaySubject, Subject, takeUntil } from 'rxjs';
+import { AppState } from 'src/app/reducers/country.reducer';
 
 @Component({
   selector: 'app-search-filter',
@@ -7,14 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFilterComponent implements OnInit {
   isShowDropdown: boolean = false;
+  inputTextChanged = new Subject<Event>();
+  countryRegions$ = new Observable<any[]>();
+  private subscriptions: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor() { }
+  @Output() searchTermEntered = new EventEmitter();
+  @Output() regionFilterSet = new EventEmitter();
+
+  constructor(private store: Store<AppState>) {
+    this.countryRegions$ = store.select(state => (state.countries as any).countries)
+      .pipe(map(countries => {
+        return [...new Set(countries.map(country => country.subRegion))]
+      }))
+  }
 
   ngOnInit(): void {
+    this.inputTextChanged.pipe(
+      map(e => (e.target as HTMLInputElement).value),
+      debounceTime(100),
+      takeUntil(this.subscriptions)
+    ).subscribe(text => {
+      this.searchTermEntered.emit(text);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.next(true);
+    this.subscriptions.complete();
   }
 
   toggleDropdown(): void {
     this.isShowDropdown = !this.isShowDropdown;
+  }
+
+  inputChanged(inputChangeEvent: Event) {
+    this.inputTextChanged.next(inputChangeEvent);
+  }
+
+  filterByRegion(region: string) {
+    this.regionFilterSet.emit(region);
   }
 
 }
