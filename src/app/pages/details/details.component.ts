bb2a19c0b5ef6cd1588e177b07e0ebb74a8a9efd
Gerald Nnebe <34799566+SabreFoxx@users.noreply.github.com>
2022-02-09 00:02:17 +0100

    view and click border countries closes #24

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { map, Observable, pluck, ReplaySubject, takeUntil } from 'rxjs';
+import { AppState } from 'src/app/reducers/country.reducer';
+import { Country } from 'src/models/country.model';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
+  country$ = new Observable<Country>();
+  neighbors$ = new Observable<Country[]>();
+  allCountries$ = new Observable<Country[]>();
+  private borders = new Array<string>();
+  private subscriptions$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private store: Store<AppState>) {
+    this.allCountries$ = store.select(state => (state.countries as any).countries);
+  }
 
   ngOnInit(): void {
+    // TODO action is called twice
+    this.store.dispatch({ type: '[COUNTRY] Load' });
+
+    let countryCode = this.route.snapshot.paramMap.get('countryCode');
+    this.country$ = this.allCountries$.pipe(
+      map(countries => countries.find(c => c.code == countryCode))
+    );
+
+    this.country$.pipe(
+      takeUntil(this.subscriptions$)
+    ).subscribe(country => this.borders = country?.borders);
+
+    this.neighbors$ = this.allCountries$.pipe(
+      map(countries => countries.filter(c => this.borders.includes(c.code)))
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions$.next(true);
+    this.subscriptions$.complete();
   }
 
 }
--- src/services/country.service.ts
+++ src/services/country.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/reducers/country.reducer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private store: Store<AppState>) {
+    this.store.dispatch({ type: '[COUNTRY] Load' });
+  }
 
   getAll() {
     return this.http.get<any[]>('https://restcountries.com/v3.1/all');
